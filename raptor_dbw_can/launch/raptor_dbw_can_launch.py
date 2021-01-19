eaff73d1d6d5fdec8bd0eb3a3b034ca98974ff73
Joshua Whitley <josh.whitley@autoware.org>
2021-01-18 19:27:13 -0600

    Fixing Python formatting.

--- raptor_dbw_can/launch/raptor_dbw_can_launch.py
+++ raptor_dbw_can/launch/raptor_dbw_can_launch.py
@@ -2,18 +2,18 @@
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright
 #   notice, this list of conditions and the following disclaimer.
-# 
+#
 # * Redistributions in binary form must reproduce the above copyright
 #   notice, this list of conditions and the following disclaimer in the
 #   documentation and/or other materials provided with the distribution.
-# 
+#
 # * Neither the name of the {copyright_holder} nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -52,14 +52,16 @@ from launch.substitutions import LaunchConfiguration
 from launch.substitutions import ThisLaunchFileDir
 from ament_index_python import get_package_share_directory
 
+
 def generate_launch_description():
     params_file = LaunchConfiguration(
         'params',
         default=[ThisLaunchFileDir(), '/launch_params.yaml'])
 
-    #make sure the dbc file gets installed with the launch file
-    dbc_file_path = get_package_share_directory('raptor_dbw_can') + "/launch/New_Eagle_DBW_3.3.388.dbc"
-    
+    # make sure the dbc file gets installed with the launch file
+    dbc_file_path = get_package_share_directory('raptor_dbw_can') + \
+        "/launch/New_Eagle_DBW_3.3.388.dbc"
+
     return LaunchDescription(
         [
             Node(
@@ -78,5 +80,6 @@ def generate_launch_description():
                 namespace='',
                 parameters=[params_file]),
         ])
+
 
 generate_launch_description()
